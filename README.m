e4707dc1f4d976eeba800d8f3c03e0d5ccbe3ffa
Iamsanika28 <sanikak569@gmail.com>
2024-10-02 16:20:42 +0530

    Delete README.m

--- README.m
+++
@@ -1,7 +0,0 @@
-#include <iostream> it is a header file of a program.
-Using namespace std it is our template.
-int main it is our main function 
-int number it is an integer variable which store input of a program.
-cout it console output.enter an integer asking for user to enter the Integer.
-cin it is console input and number store in input value.
-Then for our output  we use cout you entered means we get output as which number we have print mean we print 8 then output will get 8.

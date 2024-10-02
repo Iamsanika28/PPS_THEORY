70bf744b226dfaf7670438d537690ed64e017d40
Iamsanika28 <sanikak569@gmail.com>
2024-10-02 17:10:06 +0530

    Add files via upload
    This program is for the use of logical operators (&&, ||, !) in C++.#include<iostream> it is header file.using namespace std it is template.int main it is main function.Two integer variables a and b are declared and initialized with the values 8 and 5, respectively.  a && b` (Logical AND):`a` is 8 (non-zero, treated as true)
      `b` is 5 (non-zero, treated as true)
        - Result: true (1)
     `a || b` (Logical OR):
        - `a` is 8 (non-zero, treated as true)
        - `b` is 5 (non-zero, treated as true)
        - Result: true (1)
     `!b` (Logical NOT):
        - `b` is 5 (non-zero, treated as true)
        - Result: false (0), because `!` inverts the boolean value.
    In C++  Non-zero values are treated as true
    - Zero is treated as false
    - Logical operators return 1
    Return 0 is for successfuly execution of program.

---
+++ Program 7(Bitwise operator).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a = 8, b = 5;
+// Logical AND operator
+cout << "a && b is " << (a && b) << endl;
+// Logical OR operator
+cout << "a || b is " << (a || b) << endl;
+// Logical NOT operator
+cout << "!b is " << (!b) << endl;
+return 0;
+}
